1ec990d3a13e99257e1c11dba402a048035dce9c
Iago <igonzaleziglesias@danielcastelao.org>
2019-12-05 14:32:39 +0100

    añadido reto4

---
+++ app/src/main/java/com/dam2/proyecto/Reto4.kt
@@ -0,0 +1,12 @@
+package com.dam2.proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reto4 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reto4)
+    }
+}
